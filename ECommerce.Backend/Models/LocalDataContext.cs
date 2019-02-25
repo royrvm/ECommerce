274d2fa7d08d48e5d13399ff47f1c4859d059551
royrvm <46869929+royrvm@users.noreply.github.com>
2019-02-25 18:15:23 -0500

    Bad Created Department

---
+++ ECommerce.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Backend.Models
+{
+    using Domain.Models;
+    public class LocalDataContext:DataContext
+    {
+        public System.Data.Entity.DbSet<ECommerce.Common.Models.Department> Departments { get; set; }
+    }
+}
---
+++ ECommerce.Domain/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerce.Domain.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
